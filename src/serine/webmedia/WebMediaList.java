fb0e4ff5ff858b364b5126c6cfe89016f968876d
marscarda <52551131+marscarda@users.noreply.github.com>
2020-11-08 01:47:21 -0300

    Web Blog Objects

---
+++ src/serine/webmedia/WebMediaList.java
@@ -0,0 +1,35 @@
+package serine.webmedia;
+//**************************************************************************
+public class WebMediaList {
+    //**********************************************************************
+    public WebMediaList() {
+        //---------------------------------------------------------
+        items[0] = new WebMediaObject();
+        items[0].name = "blogarg";
+        items[0].lang  = BlogLang.SPANISH;
+        //---------------------------------------------------------
+    }
+    //**********************************************************************
+    private final int count = 1;
+    private final WebMediaObject[] items = new WebMediaObject[count];
+    //**********************************************************************
+    public WebMediaObject[] itemList () { return items; }
+    //======================================================================
+    public WebMediaObject itemByName (String name) {
+        for (int n = 0; n < count; n++) {
+            if (name.equals(items[n].name))
+                return items[n];
+        }
+        return new WebMediaObject();
+    }
+    //======================================================================
+    public WebMediaObject itemByHost (String host) {
+        for (int n = 0; n < count; n++) {
+            if (host.equals(items[n].host))
+                return items[n];
+        }
+        return new WebMediaObject();
+    }
+    //**********************************************************************
+}
+//**************************************************************************
---
+++ src/serine/webmedia/WebMediaObject.java
@@ -0,0 +1,15 @@
+package serine.webmedia;
+//**************************************************************************
+public class WebMediaObject {
+    //=============================================================
+    String name = null;
+    String host = null;
+    BlogLang lang = BlogLang.UNDEF;
+    //=============================================================
+    public String getName () { return name; }
+    public String getHost () { return host; }
+    public BlogLang getLang () { return lang; }
+    //=============================================================
+}
+//**************************************************************************
+
